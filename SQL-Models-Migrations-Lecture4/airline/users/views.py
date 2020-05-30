f542812a9a5f3e12867c62f83a4b9e81c31e9b88
elmu <eltadjibaev@gmail.com>
2020-05-30 19:03:10 +0900

    changes in views file

--- SQL-Models-Migrations-Lecture4/airline/users/views.py
+++ SQL-Models-Migrations-Lecture4/airline/users/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.urls import reverse
 
 # Create your views here.
+
 def index(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse('login'))
